23b242d5258e01e6ca4992418ae6dfbec1910b89
Moiz <moizmirza212@gmail.com>
2023-08-07 23:45:19 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CompA from './CompA.JS'
+import CompB from './CompB'
+import CompC from './CompC'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <CompA/>
+        <CompB/>
+        <CompC/>
+    </>
+  )
 }
 
-export default App;
+export default App
